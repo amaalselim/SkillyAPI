0f1e023eda9bc998adfb3f78de1863d1acccf061
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-28 00:31:59 +0200

    Add middleware to localization

--- Skilly.API/Program.cs
+++ Skilly.API/Program.cs
@@ -1,3 +1,7 @@
+﻿
+using Microsoft.OpenApi.Models;
+using Skilly.Application.Middlewares;
+using System.Globalization;
 
 namespace Skilly.API
 {
@@ -6,15 +10,66 @@ namespace Skilly.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
             // Add services to the container.
+            builder.Services.AddCors(corsOptions =>
+            {
+                corsOptions.AddPolicy("MyPolicy", policyBuilder =>
+                {
+                    policyBuilder.AllowAnyOrigin()
+                      .AllowAnyMethod()
+                      .AllowAnyHeader();
+                });
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            /*-----------------------------Swagger Part-----------------------------*/
+            #region Swagger REgion
+
+            // إضافة خدمات Swagger
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Laza API", Version = "v1" });
+
+                // Add support for bearer token authentication
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter 'Bearer' followed by a space and the token.",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+                });
+            });
+
+            #endregion
+
             var app = builder.Build();
+
+           app.UseMiddleware<LocalizationMiddleware>();    
+            app.UseRequestLocalization(options =>
+            {
+                options.SetDefaultCulture("ar-EG")
+                       .AddSupportedCultures("ar-EG", "en-US")
+                       .AddSupportedUICultures("ar-EG", "en-US");
+            });
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -23,6 +78,14 @@ namespace Skilly.API
                 app.UseSwaggerUI();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseCors("MyPolicy");
+
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
---
+++ Skilly.Application/Middlewares/LocalizationMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.Middlewares
+{
+    public class LocalizationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public LocalizationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            var culture = httpContext.Request.Headers["Accept-Language"].ToString();
+            culture=string.IsNullOrEmpty(culture)? "ar-EG" : culture;
+
+            var cultureInfo=new CultureInfo(culture);
+
+            CultureInfo.DefaultThreadCurrentCulture=cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture=cultureInfo;
+            
+            await _next(httpContext);
+        }
+    }
+}
