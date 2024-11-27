0267ebdbaa2817468a6d4f039c5a96deca553e3b
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-28 01:25:38 +0200

    implement UserRepository

---
+++ Skilly.Persistence/Implementation/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skilly.Core.Entities;
+using Skilly.Persistence.Abstract;
+using Skilly.Persistence.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Persistence.Implementation
+{
+    public class UserRepository : IGenericRepository<User>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(User user)
+        {
+            await _context.users.AddAsync(user);    
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            var user= await _context.users.FindAsync(id);   
+            _context.users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.users.ToListAsync();
+        }
+
+        public async Task<User> GetByIdAsync(string id)
+        {
+            return await _context.users.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _context.users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
