346cb44865570d87272605df25aa2c7a7061f0e2
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-28 01:24:21 +0200

    add usertypeenum

---
+++ Skilly.Core/Enums/UserType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Core.Enums
+{
+    public enum UserType
+    {
+        User,
+        ServiceProvider,
+        Admin
+    }
+}
