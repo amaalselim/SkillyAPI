edd6fd392a0f7bf8f54bb145f37d295a97c6afa4
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-28 01:24:09 +0200

    add user entity

---
+++ Skilly.Core/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Skilly.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Core.Entities
+{
+    public class User :IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [RegularExpression(@"^01[0125][0-9]{8}$", ErrorMessage = "يرجى إدخال رقم هاتف مصري صحيح.")]
+        public string PhoneNumber {  get; set; }
+        public UserType UserType { get; set; }
+
+    }
+}
