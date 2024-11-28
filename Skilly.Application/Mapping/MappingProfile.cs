95cdbe5a5e31adb712aac9980668b0bd4930c88b
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-29 00:24:36 +0200

    add auto mapper

---
+++ Skilly.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Skilly.Application.DTOs;
+using Skilly.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RegisterDTO,User>()
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email)).ReverseMap();
+        }
+    }
+}
