63ca7d8d1925bdd187b8aaa25ff40e520bc49de8
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-29 00:25:18 +0200

    create interface of authservice

---
+++ Skilly.Application/Abstract/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Skilly.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.Abstract
+{
+    public interface IAuthService
+    {
+        Task<IdentityResult> RegisterAsync(RegisterDTO registerDTO);
+        Task<object> LoginAsync(LoginDTO loginDTO);
+    }
+}
