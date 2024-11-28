3f0b27c1bb3bbf0ba258ab4a0e403662fa72dc05
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-29 00:23:15 +0200

    add Services of claims and token

---
+++ Skilly.Infrastructure/Implementation/ClaimsService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Skilly.Application.Abstract;
+using Skilly.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Infrastructure.Implementation
+{
+    public class ClaimsService : IClaimsService
+    {
+        private readonly UserManager<User> _userManager;
+
+        public ClaimsService(UserManager<User> userManager)
+        {
+           _userManager = userManager;
+        }
+        public async Task<List<Claim>> GetClaimsAsync(string PhoneNumber, string userId)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId),
+                new Claim(ClaimTypes.MobilePhone, PhoneNumber),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                foreach (var role in roles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, role));
+                }
+            }
+            return claims;
+        }
+    }
+}
---
+++ Skilly.Infrastructure/Implementation/TokenService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Skilly.Application.Abstract;
+using Skilly.Infrastructure.Abstract;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Infrastructure.Implementation
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _config;
+
+        public TokenService(IConfiguration config )
+        {
+            _config = config;
+        }
+        public async Task<string> CreateTokenAsync(IEnumerable<Claim> claims, bool rememberMe)
+        {
+            SecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var tokenExpiration = rememberMe ? DateTime.Now.AddDays(30) : DateTime.Now.AddHours(20);
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: _config["JWT:Issuer"],
+                audience: _config["JWT:Audience"],
+                expires: tokenExpiration,
+                claims: claims,
+                signingCredentials: signingCredentials
+            );
+
+            return await Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
