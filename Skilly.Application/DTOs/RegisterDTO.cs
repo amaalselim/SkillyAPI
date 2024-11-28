5954e673bcb3ef4cb53e6ccbe1b6d205d632579b
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-29 00:24:20 +0200

    create registerdto

---
+++ Skilly.Application/DTOs/RegisterDTO.cs
@@ -0,0 +1,22 @@
+﻿using Skilly.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs
+{
+    public class RegisterDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [RegularExpression(@"^01[0125][0-9]{8}$", ErrorMessage = "يرجى إدخال رقم هاتف مصري صحيح.")]
+        public string PhoneNumber { get; set; }
+        [EmailAddress(ErrorMessage = "صيغة البريد الإلكتروني غير صحيحة.")]
+        public string Email  { get; set; }
+        public string Password { get; set; }
+    }
+}
