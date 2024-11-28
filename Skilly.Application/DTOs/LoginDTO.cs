9fcb981142f729e742433087c073a2f03e331d86
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-29 00:24:12 +0200

    create logindto

---
+++ Skilly.Application/DTOs/LoginDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs
+{
+    public class LoginDTO
+    {
+        [RegularExpression(@"^01[0125][0-9]{8}$", ErrorMessage = "يرجى إدخال رقم هاتف مصري صحيح.")]
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
+    }
+}
