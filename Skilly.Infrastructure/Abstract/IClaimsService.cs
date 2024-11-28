b27261a41d815ab0e4a5382edec161ca02f1f6cb
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-29 00:24:00 +0200

    implement interface of Claims and tokens

---
+++ Skilly.Infrastructure/Abstract/IClaimsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.Abstract
+{
+    public interface IClaimsService
+    {
+        Task<List<Claim>> GetClaimsAsync(string PhoneNumber, string userId);
+    }
+}
---
+++ Skilly.Infrastructure/Abstract/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Infrastructure.Abstract
+{
+    public interface ITokenService
+    {
+        Task<string> CreateTokenAsync(IEnumerable<Claim> claims, bool rememberMe);
+    }
+}
