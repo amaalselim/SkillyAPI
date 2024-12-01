ce3a1ad70f25a9f99a3de6da8fc88c435cdc2295
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-02 01:23:15 +0200

    create Verficationcode dto

---
+++ Skilly.Application/DTOs/VerficationCodeDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs
+{
+    public class VerficationCodeDTO
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string email { get; set; }
+        [Required(ErrorMessage = "VerficationCode is required.")]
+        public string code { get; set; }
+    }
+}
