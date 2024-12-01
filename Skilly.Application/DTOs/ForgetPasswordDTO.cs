bc95f74d539e787131fb77109e7c346cd0d69877
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-02 01:23:53 +0200

    create fogetpass dto

---
+++ Skilly.Application/DTOs/ForgetPasswordDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs
+{
+    public class ForgetPasswordDTO
+    {
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+    }
+}
