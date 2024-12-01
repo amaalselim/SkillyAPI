53f31440a3986538a5a2990c6ae36dd839eac772
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-02 01:23:34 +0200

    create updatepass dto

---
+++ Skilly.Application/DTOs/UpdatePasswordDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs
+{
+    public class UpdatePasswordDTO
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "New password is required.")]
+        [StringLength(100, ErrorMessage = "Password must be at least 6 characters long.", MinimumLength = 6)]
+        public string NewPassword { get; set; }
+
+        [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
