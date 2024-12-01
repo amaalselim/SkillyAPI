6a8c0399b018de634163cc545c3a7efdc7bb3875
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-02 01:24:06 +0200

    create usertype dto

---
+++ Skilly.Application/DTOs/UserTypeRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using Skilly.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs
+{
+    public class UserTypeRequestDTO
+    {
+        public UserType UserType { get; set; }
+    }
+}
