c023cba02244b098aa766ff21414cd2bef422788
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-02 01:24:19 +0200

    add migrations

---
+++ Skilly.Persistence/Migrations/20241201214416_vcode.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Skilly.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class vcode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "verificationCode",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "verificationCode",
+                table: "AspNetUsers");
+        }
+    }
+}
