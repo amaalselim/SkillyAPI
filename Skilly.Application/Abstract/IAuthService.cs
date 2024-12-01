307225d4ded2665c9881aa70e16728e9bd1241b3
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-02 01:22:56 +0200

    add actions

--- Skilly.Application/Abstract/IAuthService.cs
+++ Skilly.Application/Abstract/IAuthService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Skilly.Application.DTOs;
+using Skilly.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,10 @@ namespace Skilly.Application.Abstract
     {
         Task<IdentityResult> RegisterAsync(RegisterDTO registerDTO);
         Task<object> LoginAsync(LoginDTO loginDTO);
+        Task<IdentityResult> UpdatePasswordAsync(UpdatePasswordDTO updatePasswordDTO);
+        Task SendResetPasswordEmailAsync(string email);
+        Task<string> GeneratePasswordResetTokenAsync(ForgetPasswordDTO forgetPasswordDTO);
+        Task<User> FindByEmailAsync(string email);
+        Task<bool> VerifyEmailCodeAsync(VerficationCodeDTO verficationCodeDTO);
     }
 }
--- Skilly.Application/Implementation/AuthService.cs
+++ Skilly.Application/Implementation/AuthService.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Skilly.Application.Abstract;
 using Skilly.Application.DTOs;
 using Skilly.Core.Entities;
 using Skilly.Infrastructure.Abstract;
+using Skilly.Infrastructure.Implementation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,24 +23,35 @@ namespace Skilly.Application.Implementation
         private readonly ITokenService _tokenService;
         private readonly IClaimsService _claimsService;
 
+        public IHttpContextAccessor _httpContextAccessor { get; }
+        public IEmailService _emailService { get; }
+
         public AuthService(IMapper mapper,
+            IHttpContextAccessor httpContextAccessor,
             UserManager<User> user,
             SignInManager<User> manager,
             ITokenService tokenService,
-            IClaimsService claimsService
+            IClaimsService claimsService,
+            IEmailService emailService
             )
         {
             _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
             _usermanager = user;
             _manager = manager;
             _tokenService = tokenService;
             _claimsService = claimsService;
+            _emailService = emailService;
         }
         public async Task<object> LoginAsync(LoginDTO loginDTO)
         {
             var user = await _usermanager.Users.FirstOrDefaultAsync(u => u.PhoneNumber == loginDTO.PhoneNumber);
             if (user != null)
             {
+                if (!user.EmailConfirmed) 
+                {
+                    return new { Success = false, Message = "Email not confirmed. Please verify your email first." };
+                }
                 var result = await _manager.PasswordSignInAsync(user.Email, loginDTO.Password, loginDTO.RememberMe, false);
                 if (result.Succeeded)
                 {
@@ -57,10 +70,107 @@ namespace Skilly.Application.Implementation
         }
 
 
-            public async Task<IdentityResult> RegisterAsync(RegisterDTO registerDTO)
+        public async Task<IdentityResult> RegisterAsync(RegisterDTO registerDTO)
+        {
+            var user = _mapper.Map<User>(registerDTO);
+            user.UserType = registerDTO.UserType;
+            user.EmailConfirmed = false;
+
+            var result = await _usermanager.CreateAsync(user, registerDTO.Password);
+            if (result.Succeeded)
             {
-                var user = _mapper.Map<User>(registerDTO);
-                return await _usermanager.CreateAsync(user,registerDTO.Password);
+               _httpContextAccessor. HttpContext.Session.SetString("RegistrationTime", DateTime.UtcNow.ToString());
+
+                Random random = new Random();
+                int verificationCode = random.Next(1000, 9999);
+
+                user.verificationCode = verificationCode;
+                await _usermanager.UpdateAsync(user);
+
+                await _emailService.SendEmailAsync(user.Email, user.FirstName, "Email Confirmation",
+                    $"Your email verification code is:<br/><code style='font-size: 18px; color: #3498db;'>{verificationCode}</code>");
             }
+            return result;
+        }
+
+        public async Task<string> GeneratePasswordResetTokenAsync(ForgetPasswordDTO forgetPasswordDTO)
+        {
+            var user = await _usermanager.FindByEmailAsync(forgetPasswordDTO.Email);
+            if (user == null)
+            {
+                return null;
+            }
+            return await _usermanager.GeneratePasswordResetTokenAsync(user);
+        }
+        public async Task SendResetPasswordEmailAsync(string email)
+        {
+            var user = await _usermanager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return;
+            }
+            Random random = new Random();
+            int verificationCode = random.Next(1000, 9999);
+
+            user.verificationCode = verificationCode;
+            await _usermanager.UpdateAsync(user);
+            await _emailService.SendEmailAsync(email, user.FirstName, "Reset Your Password",
+            $"Your verification code is:<br/><code style='font-size: 18px; color: #3498db;'>{verificationCode}</code>");
+        }
+        public async Task<IdentityResult> UpdatePasswordAsync(UpdatePasswordDTO updatePasswordDTO)
+        {
+            var user = await _usermanager.FindByEmailAsync(updatePasswordDTO.Email);
+            if(user == null)
+            {
+                return IdentityResult.Failed(new IdentityError { Description = "User not found." });
+            }
+            var token = await _usermanager.GeneratePasswordResetTokenAsync(user);
+            var result= await _usermanager.ResetPasswordAsync(user, token, updatePasswordDTO.NewPassword);
+
+            if (result.Succeeded)
+            {
+                user.verificationCode = null;
+                await _usermanager.UpdateAsync(user);
+            }
+            return result;
+        }
+        public async Task<User> FindByEmailAsync(string email)
+        {
+            return await _usermanager.FindByEmailAsync(email);
+        }
+        public async Task<bool> VerifyEmailCodeAsync(VerficationCodeDTO verficationCodeDTO)
+        {
+            var user = await _usermanager.FindByEmailAsync(verficationCodeDTO.email);
+            if (user == null)
+            {
+                return false;
+            }
+            string registrationTimeString = _httpContextAccessor.HttpContext.Session.GetString("RegistrationTime");
+            if (string.IsNullOrEmpty(registrationTimeString))
+            {
+                return false; 
+            }
+
+            DateTime registrationTime = DateTime.Parse(registrationTimeString);
+
+            if ((DateTime.UtcNow - registrationTime).TotalMinutes > 30)
+            {
+                await _usermanager.DeleteAsync(user);
+               _httpContextAccessor.HttpContext.Session.Remove("RegistrationTime");
+                return false;
+            }
+
+            if (user.verificationCode.ToString() == verficationCodeDTO.code)
+            {
+                user.EmailConfirmed = true;
+                user.verificationCode = null;
+                await _usermanager.UpdateAsync(user);
+                return true;  
+            }
+
+            return false;
+        }
+
+
     }
 }
