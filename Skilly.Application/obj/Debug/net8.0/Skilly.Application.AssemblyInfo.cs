99d815915a6fce6d82bf2ab246a4d59becff36e2
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-03 18:12:41 +0200

    change

--- Skilly.Application/obj/Debug/net8.0/Skilly.Application.AssemblyInfo.cs
+++ Skilly.Application/obj/Debug/net8.0/Skilly.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Skilly.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2d9e4135519ff790a9f6dba9b17799628cacd5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc0887734d6bc637d3f9f1173d2a046bcddc48c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Skilly.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Skilly.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
