eb7fb6ce458602ff6f32f9853204bcef03011117
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-04 21:44:53 +0200

    add serviceprovider controller

---
+++ Skilly.API/Controllers/ServiceProviderController.cs
@@ -0,0 +1,131 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Skilly.Application.Abstract;
+using Skilly.Application.DTOs;
+using Skilly.Application.Exceptions;
+using Skilly.Core.Entities;
+using Skilly.Persistence.Abstract;
+using System.Security.Claims;
+using ServiceProvider = Skilly.Core.Entities.ServiceProvider;
+
+namespace Skilly.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceProviderController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ServiceProviderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet("GetAllServiceProvider")]
+        public async Task<ActionResult<IEnumerable<ServiceProvider>>> GetAllServiceProvider()
+        {
+            var users = await _unitOfWork.ServiceProviderRepository.GetAllServiceProviderAsync();
+            return Ok(users);
+        }
+        [HttpGet("GetServiceProviderBy/{id}")]
+        public async Task<ActionResult<ServiceProvider>> GetUserById(string id)
+        {
+            var user = await _unitOfWork.ServiceProviderRepository.GetByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+        [HttpPost("addServiceProvider")]
+        [Authorize]
+        public async Task<IActionResult> AddServiceProvider([FromForm] ServiceProviderDTO ServiceProviderDTO)
+        {
+            try
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                if (string.IsNullOrEmpty(userId))
+                {
+                    return Unauthorized(new { message = "User not authenticated." });
+                }
+
+                await _unitOfWork.ServiceProviderRepository.AddServiceProviderAsync(ServiceProviderDTO, userId);
+
+                return Ok(new
+                {
+                    message = "User profile added successfully.",
+                    data = ServiceProviderDTO
+                });
+            }
+            catch (UserNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occurred.", error = ex.Message });
+            }
+        }
+        [HttpPut("editServiceProvider")]
+        [Authorize]
+        public async Task<IActionResult> EditServiceProvider([FromForm] ServiceProviderDTO ServiceProviderDTO)
+        {
+            try
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                if (string.IsNullOrEmpty(userId))
+                {
+                    return Unauthorized(new { message = "User not authenticated." });
+                }
+
+                await _unitOfWork.ServiceProviderRepository.EditServiceProviderAsync(ServiceProviderDTO, userId);
+
+                return Ok(new
+                {
+                    message = "User profile updated successfully.",
+                    data = ServiceProviderDTO
+                });
+            }
+            catch (UserNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+            catch (ServiceProviderNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occurred.", error = ex.Message });
+            }
+        }
+
+        [HttpDelete("deleteProviderBy/{id}")]
+        public async Task<IActionResult> DeleteServiceProvider(string id)
+        {
+            try
+            {
+
+                await _unitOfWork.ServiceProviderRepository.DeleteServiceProviderAsync(id);
+
+                return Ok(new { message = "User profile deleted successfully." });
+            }
+            catch (UserNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+            catch (ServiceProviderNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occurred.", error = ex.Message });
+            }
+        }
+
+    }
+}
