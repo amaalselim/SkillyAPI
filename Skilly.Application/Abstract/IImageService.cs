5b591e74334176e252f6763ebbb994c6072b7e9f
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-04 21:48:41 +0200

    edit savefile to save pdf

---
+++ Skilly.Application/Abstract/IImageService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.Abstract
+{
+    public interface IImageService
+    {
+        Task<string> SaveFileAsync(IFormFile Img, string folderPath);
+        Task DeleteFileAsync(string File);
+    }
+}
