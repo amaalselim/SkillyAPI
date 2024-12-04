cd9e4d6d406b4a64e4c161c04c533630c5da9f5e
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-04 21:49:42 +0200

    add auto mapper between dto and models

--- Skilly.Application/Mapping/MappingProfile.cs
+++ Skilly.Application/Mapping/MappingProfile.cs
@@ -15,6 +15,23 @@ namespace Skilly.Application.Mapping
         {
             CreateMap<RegisterDTO,User>()
             .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email)).ReverseMap();
+
+
+            CreateMap<UserProfile, UserProfileDTO>()
+            .ForMember(dest => dest.Img, opt => opt.Ignore()); 
+
+            CreateMap<UserProfileDTO, UserProfile>()
+                .ForMember(dest => dest.Img, opt => opt.MapFrom(src => src.Img != null ? src.Img.FileName : null));
+
+            CreateMap<ServiceProvider, ServiceProviderDTO>()
+            .ForMember(dest => dest.Img, opt => opt.Ignore())
+            .ForMember(dest => dest.NationalNumberPDF, opt => opt.Ignore());
+
+
+            CreateMap<ServiceProviderDTO, ServiceProvider>()
+                .ForMember(dest => dest.Img, opt => opt.MapFrom(src => src.Img != null ? src.Img.FileName : null))
+                .ForMember(dest => dest.NationalNumberPDF, opt => opt.MapFrom(src => src.NationalNumberPDF != null ? src.NationalNumberPDF.FileName : null));
         }
+
     }
 }
