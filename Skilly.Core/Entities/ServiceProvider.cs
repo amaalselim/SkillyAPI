bd20b072580af4645b44aca76d61503566135b90
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-04 21:50:07 +0200

    create model

---
+++ Skilly.Core/Entities/ServiceProvider.cs
@@ -0,0 +1,34 @@
+﻿using Skilly.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Skilly.Core.Entities
+{
+    public class ServiceProvider
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Governorate { get; set; }
+        public string City { get; set; }
+        public string StreetName { get; set; }
+        public string Age { get; set; }
+        public Gender Gender { get; set; }
+        public string Img { get; set; }
+        public string profession {  get; set; }
+        public string NumberOfYearExperience { get; set; }
+        public string BriefSummary { get; set; }
+        public string NationalNumberPDF { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        [JsonIgnore]
+        public User User { get; set; }
+    }
+}
