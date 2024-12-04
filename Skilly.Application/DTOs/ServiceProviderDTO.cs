8ef5acf3bd0f0acf6ef018a5a885b773e96b64ae
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-04 21:48:52 +0200

    add serviceproviderdto

---
+++ Skilly.Application/DTOs/ServiceProviderDTO.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Skilly.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs
+{
+    public class ServiceProviderDTO
+    {
+        public string Governorate { get; set; }
+        public string City { get; set; }
+        public string StreetName { get; set; }
+        public string Age { get; set; }
+        public Gender Gender { get; set; }
+        public IFormFile Img { get; set; }
+        public string profession { get; set; }
+        public string NumberOfYearExperience { get; set; }
+        public string BriefSummary { get; set; }
+        public IFormFile NationalNumberPDF { get; set; }
+    }
+}
