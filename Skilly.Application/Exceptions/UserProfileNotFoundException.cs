5d6428d62f07e82694e772584f7cb4e74eae094f
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-04 21:49:11 +0200

    add exceptions

---
+++ Skilly.Application/Exceptions/UserProfileNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.Exceptions
+{
+    public class UserProfileNotFoundException : Exception
+    {
+        public UserProfileNotFoundException(string message) : base(message) { }
+    }
+}
