be36763e655d9be316fd418812bba7b9f48245b4
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-07 17:07:00 +0200

    add gallery and galley image DbSet

--- Skilly.Persistence/DataContext/ApplicationDbContext.cs
+++ Skilly.Persistence/DataContext/ApplicationDbContext.cs
@@ -20,6 +20,8 @@ namespace Skilly.Persistence.DataContext
         public DbSet<User> users { get; set; }
         public DbSet<UserProfile> userProfiles { get; set; }
         public DbSet<ServiceProvider> serviceProviders { get; set; }
+        public DbSet<Servicesgallery> servicesgalleries { get; set; }
+        public DbSet<ServicesgalleryImage>galleryImages { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=GraduationProject-Skilly;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
