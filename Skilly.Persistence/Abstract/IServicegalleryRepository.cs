b10c5fd9818bc49cb0f6b288f419dfa7abe660e1
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-07 17:06:42 +0200

    create interface for gallery

---
+++ Skilly.Persistence/Abstract/IServicegalleryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Skilly.Application.DTOs;
+using Skilly.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Persistence.Abstract
+{
+    public interface IServicegalleryRepository 
+    {
+        Task<IEnumerable<Servicesgallery>> GetAllServiceGallery();
+        Task<Servicesgallery> GetServiceGalleryByIdAsync(string galleryId, string userId);
+        Task AddServiceGallery(servicegalleryDTO servicegalleryDTO, string userId);
+        Task EditServiceGallery(servicegalleryDTO servicegalleryDTO, string userId, string galleryId);
+        Task DeleteServiceGalleryAsync(string galleryId, string userId);
+    }
+}
