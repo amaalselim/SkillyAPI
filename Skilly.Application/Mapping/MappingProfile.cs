2d6a066eebb7db420b4a02dfec8de868cd43bb8a
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-07 17:06:00 +0200

    add mapping for serivce gallery

--- Skilly.Application/Mapping/MappingProfile.cs
+++ Skilly.Application/Mapping/MappingProfile.cs
@@ -1,37 +1,50 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Skilly.Application.DTOs;
 using Skilly.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Skilly.Application.Mapping
+public class MappingProfile : Profile
 {
-    public class MappingProfile : Profile
+    public MappingProfile()
     {
-        public MappingProfile()
-        {
-            CreateMap<RegisterDTO,User>()
-            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email)).ReverseMap();
+        CreateMap<RegisterDTO, User>()
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email))
+            .ReverseMap();
 
+        CreateMap<UserProfile, UserProfileDTO>()
+            .ForMember(dest => dest.Img, opt => opt.Ignore());
 
-            CreateMap<UserProfile, UserProfileDTO>()
-            .ForMember(dest => dest.Img, opt => opt.Ignore()); 
+        CreateMap<UserProfileDTO, UserProfile>()
+            .ForMember(dest => dest.Img, opt => opt.MapFrom(src => src.Img != null ? src.Img.FileName : null));
 
-            CreateMap<UserProfileDTO, UserProfile>()
-                .ForMember(dest => dest.Img, opt => opt.MapFrom(src => src.Img != null ? src.Img.FileName : null));
-
-            CreateMap<ServiceProvider, ServiceProviderDTO>()
+        CreateMap<ServiceProvider, ServiceProviderDTO>()
             .ForMember(dest => dest.Img, opt => opt.Ignore())
             .ForMember(dest => dest.NationalNumberPDF, opt => opt.Ignore());
 
+        CreateMap<ServiceProviderDTO, ServiceProvider>()
+            .ForMember(dest => dest.Img, opt => opt.MapFrom(src => src.Img != null ? src.Img.FileName : null))
+            .ForMember(dest => dest.NationalNumberPDF, opt => opt.MapFrom(src => src.NationalNumberPDF != null ? src.NationalNumberPDF.FileName : null));
 
-            CreateMap<ServiceProviderDTO, ServiceProvider>()
-                .ForMember(dest => dest.Img, opt => opt.MapFrom(src => src.Img != null ? src.Img.FileName : null))
-                .ForMember(dest => dest.NationalNumberPDF, opt => opt.MapFrom(src => src.NationalNumberPDF != null ? src.NationalNumberPDF.FileName : null));
-        }
+        CreateMap<servicegalleryDTO, Servicesgallery>()
+            .ForMember(dest => dest.Images, opt => opt.MapFrom(src => MapImages(src.Images))); 
 
+        CreateMap<Servicesgallery, servicegalleryDTO>()
+            .ForMember(dest => dest.serviceProviderName, opt => opt.MapFrom(src => src.serviceProvider != null ? src.serviceProvider.FirstName + " " + src.serviceProvider.LastName : "NUll"))
+            .ForMember(dest => dest.Images, opt => opt.MapFrom(src => src.Images.Select(img => new servicegalleryImageDTO
+            {
+                Img = img.Img
+            }).ToList()));
+
+    }
+
+    private List<ServicesgalleryImage> MapImages(IEnumerable<IFormFile> images)
+    {
+        if (images == null || !images.Any())
+            return new List<ServicesgalleryImage>();
+
+        return images.Select(img => new ServicesgalleryImage
+        {
+            Img = img.FileName 
+        }).ToList();
     }
 }
