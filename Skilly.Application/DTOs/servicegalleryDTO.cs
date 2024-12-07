48c3117819e545b941a2524e8f709c9205fc3929
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-07 17:03:32 +0200

    add service gallery dto

---
+++ Skilly.Application/DTOs/servicegalleryDTO.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs
+{
+    public class servicegalleryDTO
+    {
+        public string galleryName { get; set; }
+        public string Description { get; set; }
+        public string Deliverytime { get; set; }
+        public IFormFile Img { get; set; }
+        public List<IFormFile> Images { get; set; } = new List<IFormFile>();
+
+        public string? serviceProviderId { get; set; }
+        public string? serviceProviderName { get; set; }
+
+    }
+}
