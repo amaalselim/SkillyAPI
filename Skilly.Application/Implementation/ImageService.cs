56b13103d0173e46f9959fb971d8d2c747f491c8
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-07 17:05:45 +0200

    edit on save image to save video

--- Skilly.Application/Implementation/ImageService.cs
+++ Skilly.Application/Implementation/ImageService.cs
@@ -34,41 +34,36 @@ namespace Skilly.Application.Implementation
         {
             if (file == null || file.Length == 0)
             {
-                return null;
+                throw new ArgumentException("File is empty or null.");
             }
 
             string extension = Path.GetExtension(file.FileName).ToLower();
-
             string fullFolderPath = Path.Combine(_webHostEnvironment.WebRootPath, folderPath);
 
             if (!Directory.Exists(fullFolderPath))
             {
                 Directory.CreateDirectory(fullFolderPath);
             }
-            if (extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".gif")
+
+            var allowedExtensions = new List<string> { ".jpg", ".jpeg", ".png", ".gif", ".pdf", ".mp4", ".mov", ".avi", ".mkv", ".m4v" };
+
+            if (!allowedExtensions.Contains(extension))
             {
-                string filePath = Path.Combine(fullFolderPath, Guid.NewGuid() + extension);
-                using (var fileStream = new FileStream(filePath, FileMode.Create))
-                {
-                    await file.CopyToAsync(fileStream);
-                }
-                return Path.Combine(folderPath, Path.GetFileName(filePath));
+                throw new InvalidOperationException("Only image, video, or PDF files are allowed.");
             }
 
-            else if (extension == ".pdf")
+            string filePath = Path.Combine(fullFolderPath, file.FileName);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
             {
-                string filePath = Path.Combine(fullFolderPath, Guid.NewGuid() + extension); // اسم فريد
-                using (var fileStream = new FileStream(filePath, FileMode.Create))
-                {
-                    await file.CopyToAsync(fileStream);
-                }
-                return Path.Combine(folderPath, Path.GetFileName(filePath));
+                await file.CopyToAsync(fileStream);
             }
-            else
-            {
-                throw new InvalidOperationException("Only image or PDF files are allowed.");
-            }
+
+
+            return Path.Combine(folderPath, file.FileName); 
         }
+
+
 
 
     }
