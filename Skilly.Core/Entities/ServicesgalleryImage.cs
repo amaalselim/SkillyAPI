35f4500daa8a3940d68242f5b37fef844016968c
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-07 17:06:25 +0200

    add service gallery model

---
+++ Skilly.Core/Entities/ServicesgalleryImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Skilly.Core.Entities
+{
+    public class ServicesgalleryImage
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Img {  get; set; }
+        [ForeignKey("Servicesgallery")]
+        public string galleryId { get; set; }
+        [JsonIgnore]
+        public Servicesgallery? Servicesgallery { get; set; }
+    }
+}
