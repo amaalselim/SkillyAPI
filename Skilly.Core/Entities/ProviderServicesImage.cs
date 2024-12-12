38db9cebb5f4d574810f61b5ce6aa9dbc7dcf2bd
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-13 00:47:35 +0200

    add sevicesImage Model

---
+++ Skilly.Core/Entities/ProviderServicesImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Skilly.Core.Entities
+{
+    public class ProviderServicesImage
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Img { get; set; }
+        [ForeignKey("ProviderServices")]
+        public string serviceId { get; set; }
+        [JsonIgnore]
+        public ProviderServices? ProviderServices { get; set; }
+    }
+}
