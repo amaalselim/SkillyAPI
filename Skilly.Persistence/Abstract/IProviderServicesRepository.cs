7bddd24be730fe0d8b99bc42e666f95a03a17543
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-13 00:50:11 +0200

    add Interface ProviderServices

---
+++ Skilly.Persistence/Abstract/IProviderServicesRepository.cs
@@ -0,0 +1,19 @@
+﻿using Skilly.Application.DTOs;
+using Skilly.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Persistence.Abstract
+{
+    public interface IProviderServicesRepository
+    {
+        Task<IEnumerable<ProviderServices>> GetAllProviderService();
+        Task<ProviderServices> GetProviderServiceByIdAsync(string galleryId, string userId);
+        Task AddProviderService(ProviderservicesDTO providerservicesDTO, string userId);
+        Task EditProviderService(ProviderservicesDTO providerservicesDTO, string userId, string serviceId);
+        Task DeleteProviderServiceAsync(string serviceId, string userId);
+    }
+}
