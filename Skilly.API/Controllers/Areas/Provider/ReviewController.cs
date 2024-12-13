f9682a5bfa8434c2f3eb357feb64644292731a7d
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-13 21:23:26 +0200

    Create ReviewController

---
+++ Skilly.API/Controllers/Areas/Provider/ReviewController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Skilly.Application.DTOs;
+using Skilly.Persistence.Abstract;
+using System.Security.Claims;
+
+namespace Skilly.API.Controllers.Areas.Provider
+{
+    [Route("api/Provider/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ReviewController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        private string GetUserIdFromClaims()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userId))
+            {
+                throw new UnauthorizedAccessException("User not authorized.");
+            }
+            return userId;
+        }
+
+        [HttpPost("AddReview")]
+        public async Task<IActionResult> AddReview([FromBody] ReviewDTO reviewDTO)
+        {
+            if (reviewDTO == null)
+            {
+                return BadRequest(new { message = "Invalid review data." });
+            }
+
+            try
+            {
+                string userId = GetUserIdFromClaims();
+                await _unitOfWork.reviewRepository.AddReviewAsync(userId, reviewDTO);
+
+                return Ok(new { message = "Review added successfully.", data = reviewDTO });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = $"Internal server error: {ex.Message}" });
+            }
+        }
+
+        [HttpGet("GetReviewsBy/{providerId}")]
+        public async Task<IActionResult> GetAllReviewsByProvider([FromRoute] string providerId)
+        {
+            if (string.IsNullOrEmpty(providerId))
+            {
+                return BadRequest(new { message = "Provider ID is required." });
+            }
+
+            try
+            {
+                var reviews = await _unitOfWork.reviewRepository.GetAllReviewsByProviderIdAsync(providerId);
+                return Ok(new { reviews });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = $"Internal server error: {ex.Message}" });
+            }
+        }
+    }
+}
