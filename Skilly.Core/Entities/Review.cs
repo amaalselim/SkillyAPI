924e333f955c44f76179a918f1d8e9351bb4f7f4
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-13 21:21:10 +0200

    create ReviewModel

---
+++ Skilly.Core/Entities/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Core.Entities
+{
+    public class Review
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Feedback { get; set; }
+        [Range(0.0, 5.0)]
+        public decimal Rating { get; set; }
+        [ForeignKey("ServiceProvider")]
+        public string ProviderId { get; set; }
+        public virtual ServiceProvider? ServiceProvider { get; set; }
+
+    }
+}
