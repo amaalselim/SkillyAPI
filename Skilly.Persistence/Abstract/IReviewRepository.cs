d76c5a6025f1add81459b5341bfb25f90836b43b
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-13 21:22:06 +0200

    Implement Interface Review

---
+++ Skilly.Persistence/Abstract/IReviewRepository.cs
@@ -0,0 +1,16 @@
+﻿using Skilly.Application.DTOs;
+using Skilly.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Persistence.Abstract
+{
+    public interface IReviewRepository
+    {
+        Task AddReviewAsync(string userId,ReviewDTO reviewDTO);
+        Task<IEnumerable<ReviewDTO>> GetAllReviewsByProviderIdAsync(string ProviderId);  
+    }
+}
