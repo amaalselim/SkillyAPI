ef8604c756cb16fe9fc41b39c2a003728971a25d
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-13 21:21:20 +0200

    create reviewDTO

---
+++ Skilly.Application/DTOs/ReviewDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs
+{
+    public class ReviewDTO
+    {
+        public string ProviderId { get; set; }
+        public string? UserId { get; set; }
+        public string? Username { get; set; }
+        public string Feedback { get; set; }
+        public decimal Rating { get; set; }
+    }
+}
