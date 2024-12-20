c7c00f3b5f44fb5f667a52ee92f220c604c4f47c
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-20 20:13:24 +0200

    Create Notification Model

---
+++ Skilly.Core/Entities/Notifications.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Core.Entities
+{
+    public class Notifications
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public virtual User? User { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }  
+        public bool IsRead { get; set; } = false; 
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+
+        /*
+         * هسيبه دلوقتي عشان عايزه اعرف هبعت ليوزرز مييين بالظبط ولسا معملتش كاتيجوريزز
+         */
+    }
+}
