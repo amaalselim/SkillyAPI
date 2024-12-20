8060f1d2f78e66c8b7cc5558355f0352f6052502
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-20 22:55:39 +0200

    create interface for firebasegoogle

---
+++ Skilly.Persistence/Abstract/IFirebaseAuthService.cs
@@ -0,0 +1,15 @@
+﻿using Skilly.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.Abstract
+{
+    public interface IFirebaseAuthService
+    {
+        Task<FirebaseUserInfoDTO> VerifyGoogleTokenAsync (string idToken);
+
+    }
+}
