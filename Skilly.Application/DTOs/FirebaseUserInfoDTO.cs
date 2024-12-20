e725aa2e2d0a50d4a2a8e76dd33c9d6248c3fc01
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-12-20 22:54:54 +0200

    add firebase userInfoDTO

---
+++ Skilly.Application/DTOs/FirebaseUserInfoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs
+{
+    public class FirebaseUserInfoDTO
+    {
+        public string Uid { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string JwtToken { get; set; }
+    }
+}
