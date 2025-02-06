a0311dc0bb6370e3e17776916e6155b52c514b6c
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-02-06 22:53:37 +0200

    create a requestServiceImage Model

---
+++ Skilly.Core/Entities/requestServiceImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Skilly.Core.Entities
+{
+    public class requestServiceImage
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Img { get; set; }
+        [ForeignKey("RequestService")]
+        public string requestServiceId { get; set; }
+        [JsonIgnore]
+        public virtual RequestService? RequestService { get; set; }
+    }
+}
