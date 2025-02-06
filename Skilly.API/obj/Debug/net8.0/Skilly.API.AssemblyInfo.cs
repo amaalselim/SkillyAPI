e38f27cce1ba03415c0b835d3ed0f00006a1d5b1
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-02-06 02:59:13 +0200

    changes

--- Skilly.API/obj/Debug/net8.0/Skilly.API.AssemblyInfo.cs
+++ Skilly.API/obj/Debug/net8.0/Skilly.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Skilly.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ac911eb81efd0c2e93458f179c679656eb5c71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eab20bda4add640e2d5cdcf397ea909c7bf3026")]
 [assembly: System.Reflection.AssemblyProductAttribute("Skilly.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Skilly.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
