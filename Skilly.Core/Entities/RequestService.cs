c478d181d76edeb80b1047f78951df2bef99d0ae
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-02-06 22:53:21 +0200

    create a requestService Model

---
+++ Skilly.Core/Entities/RequestService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Skilly.Core.Entities
+{
+    public class RequestService
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name {  get; set; }
+        public decimal Price { get; set; }
+        public string Deliverytime { get; set; }
+        public DateTime startDate { get; set; }
+        [ForeignKey("Category")]
+        public string categoryId { get; set; }
+        public virtual Category? Category { get; set; }
+        public string? Notes { get; set; }
+        [ForeignKey("UserProfile")]
+        public string userId { get; set; }
+        public virtual UserProfile? UserProfile { get; set; }
+        public ICollection<requestServiceImage>? requestServiceImages{ get; set; } = new List<requestServiceImage>();
+
+    }
+}
