9e1824811493a764c01e313aeb6274488f1394e9
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-02-07 16:08:28 +0200

    add Migration

---
+++ Skilly.Persistence/Migrations/20250207134609_requestServices.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Skilly.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class requestServices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "requestServices",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Deliverytime = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    startDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    categoryId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    userId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_requestServices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_requestServices_categories_categoryId",
+                        column: x => x.categoryId,
+                        principalTable: "categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_requestServices_userProfiles_userId",
+                        column: x => x.userId,
+                        principalTable: "userProfiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "requestServiceImages",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Img = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    requestServiceId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_requestServiceImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_requestServiceImages_requestServices_requestServiceId",
+                        column: x => x.requestServiceId,
+                        principalTable: "requestServices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_requestServiceImages_requestServiceId",
+                table: "requestServiceImages",
+                column: "requestServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_requestServices_categoryId",
+                table: "requestServices",
+                column: "categoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_requestServices_userId",
+                table: "requestServices",
+                column: "userId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "requestServiceImages");
+
+            migrationBuilder.DropTable(
+                name: "requestServices");
+        }
+    }
+}
