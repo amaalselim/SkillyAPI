d6089265c1bfb2b02a7a7dda243aff7c88a3b99b
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-02-07 15:25:10 +0200

    create an interface of requestservice

---
+++ Skilly.Persistence/Abstract/IRequestserviceRepository.cs
@@ -0,0 +1,22 @@
+﻿using Skilly.Application.DTOs;
+using Skilly.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Persistence.Abstract
+{
+    public interface IRequestserviceRepository
+    {
+        Task<IEnumerable<RequestService>> GetAllRequests();
+        Task<IEnumerable<RequestService>> GetAllRequestsByUserId(string userId);
+        Task<RequestService> GetRequestById(string requestId, string userId);
+        Task AddRequestService(requestServiceDTO requestServiceDTO, string userId);
+        Task EditRequestService(requestServiceDTO requestServiceDTO, string userId, string requestId);
+        Task DeleteRequestServiceAsync(string requestId, string userId);
+
+
+    }
+}
