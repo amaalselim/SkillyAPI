164f4bee563b936ffeecc23659900be885082ba3
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-02-07 15:24:34 +0200

    Create requestservices Controller

---
+++ Skilly.API/Controllers/Areas/userProfile/requestServicesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Skilly.API.Controllers.Areas.userProfile
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class requestServicesController : ControllerBase
+    {
+        public requestServicesController()
+        {
+            
+        }
+    }
+}
