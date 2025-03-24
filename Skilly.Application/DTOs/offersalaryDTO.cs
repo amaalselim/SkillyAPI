40dfc8e5fe2fd7b92f3d01028e8932db349c9c42
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-03-25 00:17:48 +0200

    add DTO of offerSalary

---
+++ Skilly.Application/DTOs/offersalaryDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs
+{
+    public class offersalaryDTO
+    {
+        public string ID { get; set; }
+        public decimal Salary { get; set; }
+        public string Deliverytime { get; set; }
+        public string? Notes { get; set; }
+        public string? serviceId { get; set; }
+        public string? requestserviceId { get; set; }
+        public string? ServiceName { get; set; }
+        public string? RequestServiceName { get; set; }
+
+    }
+}
