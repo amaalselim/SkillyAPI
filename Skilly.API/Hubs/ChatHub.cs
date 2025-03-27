7100d75a8c1bfb3e29d24b509dcbd2fd7262f71e
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-03-27 17:04:01 +0200

    create chathub of SignalR

---
+++ Skilly.API/Hubs/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Skilly.Core.Entities;
+using Skilly.Persistence.DataContext;
+using System.Runtime.CompilerServices;
+
+namespace Skilly.API.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ChatHub(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task SendMessage(string SenderId,string receiverId,string message)
+        {
+            var msg = new Message
+            {
+                SenderId = SenderId,
+                ReceiverId = receiverId,
+                Content = message
+            };
+
+            _context.messages.Add(msg);
+            await _context.SaveChangesAsync();
+
+            await Clients.User(receiverId).SendAsync("ReceiveMessage", SenderId, message);
+        }
+    }
+}
