eeec165076834fe90b724ecb8e0a3b394d3665ed
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-03-27 16:50:39 +0200

    create a message model for chat

---
+++ Skilly.Core/Entities/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Core.Entities
+{
+    public class Message
+    {
+        public string Id { get; set; }= Guid.NewGuid().ToString();
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; }= DateTime.UtcNow;
+    }
+}
