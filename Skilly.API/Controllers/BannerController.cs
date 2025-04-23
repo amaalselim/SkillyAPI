1e1953f7a2388020450a0da65a25af7e183443b8
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-04-23 17:20:49 +0200

    create controller of banners

---
+++ Skilly.API/Controllers/BannerController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Skilly.Application.DTOs;
+using Skilly.Persistence.Abstract;
+using Skilly.Persistence.Implementation;
+using System.Threading.Tasks;
+
+namespace Skilly.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BannerController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BannerController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("GetAllBanners")]
+        public async Task<IActionResult> GetAllBanners()
+        {
+            try
+            {
+                var banners= await _unitOfWork._BannerService.GetAllBannerAsync();
+
+                if (banners == null || !banners.Any())
+                {
+                    return NotFound(new { message = "No banners found." });
+                }
+                return Ok(new { banners });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+        [HttpPost("UploadBanner")]
+        public async Task<IActionResult> Upload([FromForm] BannerCreateDTO dto)
+        {
+            try
+            {
+                var banner = await _unitOfWork._BannerService.UploadBannerAsync(dto);
+                return Ok(banner);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
