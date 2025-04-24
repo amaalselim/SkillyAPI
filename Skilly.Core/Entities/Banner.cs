4d5356d21610f073fbbd77d755371aaa356e8ba1
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-04-24 23:05:44 +0200

    make banner nullable

--- Skilly.Core/Entities/Banner.cs
+++ Skilly.Core/Entities/Banner.cs
@@ -11,7 +11,7 @@ namespace Skilly.Core.Entities
         public int Id { get; set; }
         public string Title { get; set; }
         public string ImagePath { get; set; }
-        public string Url { get; set; }
+        public string? Url { get; set; }
 
     }
 }
