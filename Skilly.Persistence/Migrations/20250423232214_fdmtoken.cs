a8dd1fa551aa8c375b735a6b08b55ce0d172a0a9
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-04-24 23:07:28 +0200

    add migrations

---
+++ Skilly.Persistence/Migrations/20250423232214_fdmtoken.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Skilly.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class fdmtoken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FcmToken",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FcmToken",
+                table: "AspNetUsers");
+        }
+    }
+}
