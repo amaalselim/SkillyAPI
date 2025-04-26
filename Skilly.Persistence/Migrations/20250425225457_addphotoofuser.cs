ab211023c50116ed8bcfc2f609da53f50ad18311
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-04-26 15:34:45 +0300

    migrations

---
+++ Skilly.Persistence/Migrations/20250425225457_addphotoofuser.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Skilly.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addphotoofuser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ServiceRequestTime",
+                table: "requestServices",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "userImg",
+                table: "requestServices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ServiceRequestTime",
+                table: "providerServices",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "providerImg",
+                table: "providerServices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "userImg",
+                table: "requestServices");
+
+            migrationBuilder.DropColumn(
+                name: "providerImg",
+                table: "providerServices");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ServiceRequestTime",
+                table: "requestServices",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ServiceRequestTime",
+                table: "providerServices",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
