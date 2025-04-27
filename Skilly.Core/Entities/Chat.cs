617813a62735b6075864fa3f57f39f1d6ef88597
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-04-28 00:56:09 +0300

    create model of chat

---
+++ Skilly.Core/Entities/Chat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Core.Entities
+{
+    public class Chat
+    {
+        public string Id { get; set; }= Guid.NewGuid().ToString();
+
+        public string FirstUserId { get; set; }
+        [NotMapped]
+        public string FirstUserName { get; set; }
+        public virtual User FirstUser { get; set; }
+
+        public string SecondUserId { get; set; }
+        [NotMapped]
+        public string SecondUserName { get; set; }
+        public virtual User SecondUser { get; set; }
+
+        public ICollection<Message> Messages { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime LastUpdatedAt { get; set; } = DateTime.Now;
+    }
+}
