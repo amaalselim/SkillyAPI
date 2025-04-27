4c6cd48c114b25fc28360b0de21f170d407ebeb3
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-04-28 00:59:22 +0300

    changes

--- Skilly.Infrastructure/obj/Debug/net8.0/Skilly.Infrastructure.AssemblyInfo.cs
+++ Skilly.Infrastructure/obj/Debug/net8.0/Skilly.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Skilly.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d8663e8140f35de1ff59869c6a3955a0adca108")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c54a07577201af6ba71e5f5ffa2d147073439a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Skilly.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Skilly.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
