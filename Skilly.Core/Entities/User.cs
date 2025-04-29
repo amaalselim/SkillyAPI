bf03989da191893f151de74f75de7b26b431b2e2
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-04-29 14:18:39 +0300

    remove rememberMe

--- Skilly.Core/Entities/User.cs
+++ Skilly.Core/Entities/User.cs
@@ -3,6 +3,7 @@ using Skilly.Core.Enums;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
