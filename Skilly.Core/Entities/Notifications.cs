d9d4c14efc2573fa5c6739b81cda04bdbbcc88c7
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-05-03 18:04:21 +0300

    notification

--- Skilly.Core/Entities/Notifications.cs
+++ Skilly.Core/Entities/Notifications.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Skilly.Core.Entities
@@ -12,15 +13,12 @@ namespace Skilly.Core.Entities
         public string Id { get; set; } = Guid.NewGuid().ToString();
         [ForeignKey("User")]
         public string UserId { get; set; }
+        [JsonIgnore]
         public virtual User? User { get; set; }
+        
         public string Title { get; set; }
         public string Body { get; set; }  
         public bool IsRead { get; set; } = false; 
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-
-
-        /*
-         * هسيبه دلوقتي عشان عايزه اعرف هبعت ليوزرز مييين بالظبط ولسا معملتش كاتيجوريزز
-         */
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
     }
 }
