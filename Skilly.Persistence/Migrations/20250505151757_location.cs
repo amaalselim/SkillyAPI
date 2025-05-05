a57987dd0f9c38375d1bda3766e91c90eefee1a4
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-05-05 18:25:26 +0300

    add-migration

---
+++ Skilly.Persistence/Migrations/20250505151757_location.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Skilly.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class location : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "AspNetUsers");
+        }
+    }
+}
