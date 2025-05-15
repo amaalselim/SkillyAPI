8beeb474ce06b993ab2319fab843403fed7d6dd0
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-05-15 19:23:54 +0300

    add migrations

---
+++ Skilly.Persistence/Migrations/20250515150740_status-offer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Skilly.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class statusoffer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "offerSalaries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "offerSalaries");
+        }
+    }
+}
