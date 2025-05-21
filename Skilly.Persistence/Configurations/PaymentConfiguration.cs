ee652b0b6a43d688b88dfc2ba1b0bb106eeaadbb
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-05-21 03:28:00 +0300

    add payment configuration

---
+++ Skilly.Persistence/Configurations/PaymentConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Skilly.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Persistence.Configurations
+{
+    public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
+    {
+        public void Configure(EntityTypeBuilder<Payment> builder)
+        {
+            builder.ToTable("Payment");
+            builder
+            .HasOne(p => p.User)
+            .WithMany() 
+            .HasForeignKey(p => p.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        }
+
+    }
+}
