bafeed37bba1485b8cc17f9e1be1e55ba6fc66b0
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-05-26 22:44:10 +0300

    add migrations

---
+++ Skilly.Persistence/Migrations/20250526191811_makeinteger.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Skilly.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class makeinteger : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NumberOfYearExperience",
+                table: "serviceProviders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "serviceProviders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NumberOfYearExperience",
+                table: "serviceProviders",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Age",
+                table: "serviceProviders",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
