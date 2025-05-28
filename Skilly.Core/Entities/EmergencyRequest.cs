ed0f551cfaf9c24e5c348733b5ed582c1d970f27
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-05-29 02:32:17 +0300

    add emergency model

---
+++ Skilly.Core/Entities/EmergencyRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Core.Entities
+{
+    public class EmergencyRequest
+    {
+        public string Id { get; set; }= Guid.NewGuid().ToString();
+        public string UserId { get; set; }
+        public string CategoryId { get; set; }
+        public string ProblemDescription { get; set; }
+        public DateOnly RequestTime { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public string Status { get; set; } = "Pending";
+
+        public string? AssignedProviderId { get; set; }
+        
+
+
+
+    }
+}
