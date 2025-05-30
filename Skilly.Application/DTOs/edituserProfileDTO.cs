86d4d5c56ccfa081cc43a3d0c69ebf75cdbb616d
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-05-30 17:41:24 +0300

    make dto forr edit

---
+++ Skilly.Application/DTOs/edituserProfileDTO.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Skilly.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs
+{
+    public class edituserProfileDTO
+    {
+        public string Governorate { get; set; }
+        public string City { get; set; }
+        public string StreetName { get; set; }
+        public Gender Gender { get; set; }
+        public IFormFile? Img { get; set; }
+    }
+}
