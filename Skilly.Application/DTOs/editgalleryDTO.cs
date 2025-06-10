14dc34546c00fcbf457071bcd339ab7b7ed934fd
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-06-11 02:46:25 +0300

    add editgallerydto

---
+++ Skilly.Application/DTOs/editgalleryDTO.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs
+{
+    public class editgalleryDTO
+    {
+        public string galleryName { get; set; }
+        public string Description { get; set; }
+        public string Deliverytime { get; set; }
+
+        public List<string>? ImagesToDeleteIds { get; set; }
+        public List<IFormFile>? Images { get; set; }
+        public IFormFile? video { get; set; }
+    }
+}
