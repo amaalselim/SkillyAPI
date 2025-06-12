6e34701a6e4a8daad881222cdb7b25327d295d06
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-06-13 01:40:59 +0300

    edit emergency method

--- Skilly.Core/Entities/EmergencyRequest.cs
+++ Skilly.Core/Entities/EmergencyRequest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Skilly.Core.Entities
@@ -10,13 +11,29 @@ namespace Skilly.Core.Entities
     public class EmergencyRequest
     {
         public string Id { get; set; }= Guid.NewGuid().ToString();
+        [ForeignKey("User")]
         public string UserId { get; set; }
+        [NotMapped]
+        public string UserName { get; set; }
+        [JsonIgnore]
+        public virtual User? User { get; set; }
+        [ForeignKey("Category")]
         public string CategoryId { get; set; }
+        [NotMapped]
+        public string CategoryName { get; set; } 
+        [JsonIgnore]
+        public virtual Category? Category { get; set; }
         public string ProblemDescription { get; set; }
         public DateOnly RequestTime { get; set; } = DateOnly.FromDateTime(DateTime.Now);
         public string Status { get; set; } = "Pending";
 
+
+        [ForeignKey("AssignedProviderId")]
         public string? AssignedProviderId { get; set; }
+        [JsonIgnore]
+        public virtual User? AssignedProvider { get; set; }
+        [NotMapped]
+        public string AssignedProviderName { get; set; }
         public decimal? Finalprice { get; set; } = 0.0m;
 
 
