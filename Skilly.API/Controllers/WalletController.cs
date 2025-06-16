292ff8fb02c55469617f4a87930b4c52a65a13ce
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-06-16 03:34:33 +0300

    get wallet of provider endpoint

---
+++ Skilly.API/Controllers/WalletController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Skilly.Persistence.Abstract;
+
+namespace Skilly.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalletController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public WalletController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost("provider-wallet/{paymentId}")]
+        [Authorize]
+        public async Task<IActionResult> ProcessPayment(string paymentId)
+        {
+            try
+            {
+                var result = await _unitOfWork._paymentRepository.ProcessPaymentAsync(paymentId);
+                return Ok(new { result });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+       
+    }
+}
