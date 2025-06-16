9defaf6f3ce7a02ce006269cbb94ce63d148342a
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-06-16 03:35:19 +0300

    create wallet model

---
+++ Skilly.Core/Entities/Wallet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Skilly.Core.Entities
+{
+    public class Wallet
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        [ForeignKey("provider")]
+        public string ProviderId { get; set; }
+        [NotMapped]
+        public string? ProviderName { get; set; }
+        [JsonIgnore]
+        public virtual User? provider { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
