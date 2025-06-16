8ddcf93ac7cc0e9a2ccc151f2e99ac85829d121e
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-06-16 03:34:58 +0300

    add DTO's of tranactions

---
+++ Skilly.Application/DTOs/Payment/GroupedTransactionsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs.Payment
+{
+    public class GroupedTransactionsDTO
+    {
+        public string Title { get; set; } 
+        public List<TransactionDTO> Transactions { get; set; }
+    }
+}
---
+++ Skilly.Application/DTOs/Payment/TransactionDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skilly.Application.DTOs.Payment
+{
+    public class TransactionDTO
+    {
+        public string Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string FormattedCreatedAt { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public decimal Amount { get; set; }
+        public string Message { get; set; }
+    }
+}
